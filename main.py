1a2540c30dd1145b96957801feb4f3447b633f96
Tiga asa yosa <asacrypto@post.com>
2024-02-23 00:52:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import time
 import random
 import asyncio
 import datetime
@@ -17,9 +16,14 @@ async def main():
             for i in range(5000):
                 txn = await client.claim()
                 print(txn.transaction_outcome.__dict__, flush=True)  # Flush output
+                
+                current_time = datetime.datetime.now()
+                print(f'Current time: {current_time}', flush=True)  # Print current time
+                
                 delay = 60 * 60 * 4 + random.randint(10, 60)
-                print(f'\nWait for {str(datetime.timedelta(seconds=delay))}\n', flush=True)  # Flush output
-                time.sleep(delay)
+                print(f'Wait for {str(datetime.timedelta(seconds=delay))}', flush=True)  # Print wait delay
+                
+                await asyncio.sleep(delay)  # Use asyncio.sleep instead of time.sleep
                 print(f'Finished claiming at {datetime.datetime.now()}', flush=True)  # Flush output
     except Exception as e:
         print(f"An exception occurred: {e}")
